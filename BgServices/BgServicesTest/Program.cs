82e64d406ee099acc88798a3baa0381c1bef5bf0
Mohamed ARFAOUI <Mohamed.ARFAOUI@exterionmedia.fr>
2020-06-18 16:51:56 +0200

    background service console app

---
+++ BgServices/BgServicesTest/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Threading.Tasks;
+
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Hosting;
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BgServicesTest
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var builder = new HostBuilder()
+                .ConfigureAppConfiguration((hostingContext, config) =>
+                {
+                    config.AddJsonFile("appsettings.json", optional: true);
+                    config.AddEnvironmentVariables();
+
+                    if (args != null)
+                    {
+                        config.AddCommandLine(args);
+                    }
+                })
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddOptions();
+                    services.AddSingleton<IHostedService, TestService>();
+                })
+                .ConfigureLogging((hostingContext, logging) => {
+                 logging.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
+                 logging.AddConsole();
+             }); 
+
+            await builder.RunConsoleAsync();
+        }
+    }
+}
---
+++ BgServices/BgServicesTest/TestService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+using Microsoft.Extensions.Hosting;
+
+namespace BgServicesTest
+{
+    public class TestService : BackgroundService
+    {
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                Console.WriteLine("Hi from service..");
+                await Task.Delay(TimeSpan.FromSeconds(2));
+            }
+        }
+    }
+}
